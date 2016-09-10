6d48ca78981c1427efe840e03be72362503faefe
Dawid Obieglo <David@Davids-Mac-mini-2.local>
2016-09-10 13:33:25 +0100

    Initial Commit

---
+++ FamilyShopping/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FamilyShopping
+//
+//  Created by David Obieglo on 10/09/2016.
+//  Copyright © 2016 Dawid Obieglo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ FamilyShopping/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  FamilyShopping
+//
+//  Created by David Obieglo on 10/09/2016.
+//  Copyright © 2016 Dawid Obieglo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
